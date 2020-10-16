bcb39f694f2cc9b9537469b99934daa63741ec42
changan <hyx1014959288@163.com>
2020-10-16 16:43:56 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import Vant from 'vant';
 import 'vant/lib/index.css';
+import './permission'; 
 import router from '../router'
+import store from '../store'
 import App from './App.vue'
 
 const app = createApp(App)
-  app.use(Vant).use(router)
+  app.use(router).use(store).use(Vant)
   app.mount('#app')
---
+++ src/permission.js
@@ -0,0 +1,21 @@
+import router from '../router'
+import { getToken } from '../utils/auth'
+import store from '../store'
+router.beforeEach((to, from, next) => {
+  //白名单路由
+  const whiteUrl = ['/login', '/forget', '/register', '/signin_github']
+  if (getToken()) {
+    if (to.path === '/login') {
+      next('/home')
+    } else {
+      store.dispatch('GetUser')
+    }
+    next()
+  } else {
+    if (whiteUrl.includes(to.path)) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+})
